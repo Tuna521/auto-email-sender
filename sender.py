8ff898e94dc42f305261083face036264b277fd1
Tina Wang <wangzongting521@gmail.com>
2023-07-01 20:04:55 +0100

    Create simple dummy email in Outlook

---
+++ sender.py
@@ -0,0 +1,19 @@
+from appscript import app, k
+
+outlook = app('Microsoft Outlook')
+
+msg = outlook.make(
+    new=k.outgoing_message,
+    with_properties={
+        k.subject: 'Test Email',
+        k.plain_text_content: 'Test email body'})
+
+msg.make(
+    new=k.recipient,
+    with_properties={
+        k.email_address: {
+            k.name: 'Fake Person',
+            k.address: 'fakeperson@gmail.com'}})
+
+msg.open()
+msg.activate()
