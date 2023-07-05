30d7e9b71a42db684df8c48904053c4d96a507a0
Tina Wang <wangzongting521@gmail.com>
2023-07-05 16:13:58 +0100

    Extract headers from csv

--- sender.py
+++ sender.py
@@ -1,14 +1,23 @@
 from appscript import app, k
+from csv import reader
 
 outlook = app('Microsoft Outlook')
 
-f = open("email_template.html", "r")
+template_file = open("email_template.html", "r")
+
+gen_info_file = open("General_Info.csv")
+gen_info_reader = reader(gen_info_file)
+gen_info_header = []
+gen_info_header = next(gen_info_reader)
+print(gen_info_header)
+
+
 
 msg = outlook.make(
     new=k.outgoing_message,
     with_properties={
         k.subject: 'Test Email',
-        k.content: f.read()})
+        k.content: template_file.read()})
 
 msg.make(
     new=k.recipient,
